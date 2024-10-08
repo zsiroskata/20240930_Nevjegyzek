e84d8621e01248d1e36897030dd7f14f277e77a2
zsiroskata <78079466+zsiroskata@users.noreply.github.com>
2024-10-08 09:25:20 +0200

    asd

--- admin/felvitel.php
+++ admin/felvitel.php
@@ -1,154 +1,121 @@
-<?php
-if (isset($_POST["rendben"])) {
-    //változók mentés és tisztítása
-    $nev = htmlspecialchars(strip_tags(ucwords(strtolower(trim ($_POST["nev"])))));
-    $ceg = htmlspecialchars(strip_tags(ucwords(strtolower(trim ($_POST["cegnev"])))));
-    $mobil = htmlspecialchars(strip_tags(trim ($_POST["mobil"])));
-    $email = htmlspecialchars(strip_tags(trim ($_POST["email"])));
-    
-    //var_dump($nev, $ceg, $mobil, $email);
-    //kép file-ok megengedett tipusainak tárolása
-    $mime=["image/jpeg", "image/gif", "image/jpg", "image/png"];
+<?php 
 
-    if (empty($nev)) {
-        $hibak[] = "Nem adott meg nevet!"; 
-    }elseif (strlen($nev) < 2) {
-        $hibak[] = "Az ön neve gyanúsan rövid, hacsak nem kínai!!";
-    }
+if(isset($_POST['rendben'])){
 
-    if (!empty($mobil)) {
-        if (strlen($mobil) < 9) {
-            $hibak[] = "Rövid a telefonszám";
-        }/*elseif (!preg_match("/^[0-9]{9,}$/", $mobil)) {
-            $hibak[] = "A mobil szám csak számokat tartalmazhat!";
-        }*/
-    }
-    
-    if (!empty($email)) {
-        if(filter_var($email, FILTER_VALIDATE_EMAIL)) {
-            $hibak[] = "Nem megfelelő az e-mail formátum!"
-        }
-    }
+    //változók mentése és tisztítása
+    $nev = htmlspecialchars(strip_tags(ucwords(strtolower(trim($_POST['nev'])))));
+   // var_dump($nev);
+   $cegnev = htmlspecialchars(strip_tags(ucwords(strtolower(trim($_POST['cegnev'])))));
+   $mobil = htmlspecialchars(strip_tags(trim($_POST['mobil'])));
+   $email = htmlspecialchars(strip_tags(trim($_POST['email'])));
+   //print_r($cegnev);
+   //print_r($mobil);
+   //print_r($email);
 
-    if($_FILES["foto"]["error"] == 0 && $_FILES["foto"]["size"] < 2000000) {
-        $hibak[] = "Túl nagy méretű képet tölt fel max 2MB";
-    }
+   //képfájlok megengedett típusainak tárolása
+   $mime = ["image/jpeg", "image/jpg", "image/gif", "image/png"];
 
-    if ($_FILES["foto"]["error"] == 0 && !in_array($_FILES["foto"]["type"], $mime)) {
-        $hibak[] = "Nem megfelelő kép formátum!";
+   if(empty($nev)){
+    $hibak[] = "Nem adott meg nevet!";
+   }elseif(strlen($nev) < 3){
+    $hibak[] = "Az Ön neve gyanusan rövid, hacsak Ön nem kínai!";
+   }
+
+   if(!empty($mobil)){
+    if(strlen($mobil) <9 ){
+        $hibak[] = "Ön a segélyhívót akarta hívni?";
+    }/*elseif(!preg_match("/^[0-9]{9,}$/", $mobil)){
+        $hibak[] = "A mobil szám csak számokat tartalmazhat";
+    }*/
+   }
+
+   if(!empty($email)){
+    if(!filter_var($email, FILTER_VALIDATE_EMAIL)){
+        $hibak[] = "Nem megfelelő az e-mail formátuma";
+    }
+   }
+
+   if($_FILES['foto']['error'] == 0 && $_FILES['foto']['size'] > 20000000){
+        $hibak[] = "Túl nagy méretű képet tölt fel, max 2MB";
+   }
+
+   if($_FILES['foto']['error'] == 0 && !in_array($_FILES['foto']['type'], $mime))
+        $hibak[] = "Nem megfelelő képformátum!";
+
         //kép nevének összeállítása
-        switch ($_FILES['foto']['type']) {
-            case 'image/png':
-                $kit = ".png";
-                break;
-            case 'image/gif':
-                $kit = ".gif";
-                break;
-            case 'image/jpeg':
-                $kit = ".jpeg";
-                break;
-            default:
-                $kit = ".jpg"
-                break;
+        switch($_FILES['foto']['type']){
+            case "image/png": $kit = ".png"; break;
+            case "image/gif": $kit = ".gif"; break;
+            case "image/jpeg": $kit = ".jpeg"; break;
+            default: $kit = ".jpg";
         }
 
-        $foto = data("U");
-    }
+        $foto = date("U").$kit;
+        //$unix = date("U");
+       // print_r($unix);
+       //$uniqId = uniqid();
+      // print_r($uniqId);
 
-    if (!empty($email))
-    {
-        if (!filter_var($email, FILTER_VALIDATE_EMAIL))
-        {
-            $hibak[] = "Nem megfelelő az e-mail formátumnak";
-        }
-    }
-
-    if ($_FILES['foto']['error'] == 0 && $_FILES['foto']['size'] > 2000000)
-    {
-        $hibak [] = "Túl nagy méretű képet tölt fel, max 2MB";
-    }
-
-    if($_FILES['foto']['error'] == 0 && !in_array($_FILES['foto']['type'], $mime)) 
-    $hibak[] ="Nem megfelelő képformátum";
-
-    //Kép nevének összeállítása
-    switch ($_FILES["foto"]["type"])
-    {
-        case "image/png": $kit = ".png"; break;
-        case "image/gif": $kit = ".gif"; break;
-        case "image/jpeg": $kit = ".jpeg"; break;
-        default: $kit = ".jpg";
-    }
-
-    $foto = date("U") . $kit;
-    //$unix = date("U");
-    //print_r($unix);
-
-    //$uniqID = uniqid();
-    //print_r ($uniqID);
-
-    //hiba üzenetek
-    if(isset($hibak))
-    {
+      //hibaüzenetek összeállítása
+      if(isset($hibak)){
         $kimenet = "<ul>\n";
-        foreach($hibak as $hiba)
-        {
+        foreach($hibak as $hiba){
             $kimenet .= "<li>{$hiba}</li>";
         }
-        $kimenet = "</ul>\n";
-    }
-    else
-    {
+        $kimenet .= "</ul>\n";
+      }else{
         //felvitel az adatbázisba
         require "../connect.php";
         $sql = "INSERT INTO nevjegyek
-        (foto, nev, cegnev, mobil, email)
-        VALUES
-        ('{$foto}','{$nev}','{$cegnev}','{$mobile}','{$email}')";
-        mysqli_query($conn, $sql);
+                (foto, nev, cegnev, mobil, email)
+                VALUES 
+                ('{$foto}', '{$nev}','{$cegnev}', '{$mobil}','{$email}' )";
 
-        move_uploaded_file($_FILES["foto"]["tmp_name"], "../kepek/{$foto}");
-    }
+                mysqli_query($dbconn, $sql);
+
+            //kép mozgatása
+            move_uploaded_file($_FILES['foto']['tmp_name'], "../kepek/{$foto}");
+
+      }
+   
+
 }
 
-
-
-?>
-<!DOCTYPE html>
+?><!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../stilus.css">
-    <title>Új névjegyzék felvitele</title>
+    <title>Új névjegy felvitele</title>
 </head>
 <body>
     <h1>Névjegy felvitele</h1>
     <form action="" method="post" enctype="multipart/form-data">
-        <!-- hiba üzi a usernek, ha valamit elront -->
+        <!--hibaüzenetek a usernek, ha valamit rosszul tölt ki-->
+        <?php if(isset($kimenet)) print $kimenet; ?>
 
         <input type="hidden" name="MAX_FILE_SIZE" value="2000000">
         <p><label for="foto">Fotó:</label></p>
-        <input type="file" name="foto" id="foto" value="<?php if (isset($foto)) print $foto ?>">
+        <input type="file" name="foto" id="foto">
 
         <p><label for="nev">Név: *</label></p>
-        <input type="text" name="nev" id="nev" value="<?php if (isset($nev)) print $nev ?>">
-        
+        <input type="text" name="nev" id="nev" value="<?php if(isset($nev)) print $nev;?> ">
+
         <p><label for="cegnev">Cég név: *</label></p>
-        <input type="text" name="cegnev" id="cegnev">
-        
-        <p><label for="mobil">Mobil: *</label></p>
-        <input type="tel" name="mobil" id="mobil">
+        <input type="text" name="cegnev" id="cegnev" value="<?php if(isset($cegnev)) print $cegnev;?> ">
 
+        <p><label for="mobil">Telefon: *</label></p>
+        <input type="tel" name="mobil" id="mobil" value="<?php if(isset($mobil)) print $mobil;?> ">
 
-        <p><label for="email">email: *</label></p>
-        <input type="email" name="email" id="email">
+        <p><label for="email">Email: *</label></p>
+        <input type="email" name="email" id="email" value="<?php if(isset($email)) print $email;?> ">
 
-        <p><em>*-al jelölt mezők kitöltése kötelező</em></p>
+        <p><em>*-al jelöl mezők kitöltése kötelező</em></p>
 
         <input type="submit" value="Rendben" name="rendben" id="rendben">
-        <input type="reset" value="Mégse">
-
+        <input type="reset" value="Mégsem">
+    
     </form>
 </body>
 </html>
---
+++ admin/lista.php
@@ -0,0 +1,89 @@
+<?php
+require "../connect.php";
+//$kifejezes = isset($_POST["kifejezes"]) ? $_POST["kifejezes"] :"";
+//$rendez  = isset($_POST["rendez"]) ? $_POST["rendez"] :"";
+$rendez = $_POST["rendez"] ?? "nev";
+$kifejezes = $_POST["kifejezes"] ?? "";
+
+$validColumns = ["nev","cegnev","mobil","email"];
+$rendez = in_array($rendez, $validColumns) ? $rendez : "nev";
+
+$sql = "SELECT * 
+        FROM nevjegyek
+        WHERE (
+        nev LIKE ?
+        OR cegnev LIKE ?
+        OR email LIKE ?
+        OR mobil LIKE ?
+        )
+        ORDER BY {$rendez} ASC";
+
+//$result = mysqli_query($dbconn, $sql);
+$stmt = mysqli_prepare($dbconn, $sql);
+$stmt = $dbconn -> prepare($sql);
+
+if ($stmt) {
+    $searchTerm = "%{$kifejezes}%";
+    //paraméterek betöltése
+    //mysqli_stmt_bind_param($stmt,"ssss", $searchTerm, $searchTerm, $searchTerm, $searchTerm);
+    $stmt -> bind_param("ssss", $searchTerm, $searchTerm, $searchTerm, $searchTerm);
+    //utasítás végrehajtása
+    //mysqli_stmt_execute($stmt);
+    $stmt -> execute();
+    //$eredmeny = mysqli_stmt_get_result($stmt);
+    $eredmeny = $stmt -> get_result();
+    if ($eredmeny) {
+        $kimenet = "<table>
+                        <tr>
+                            <th>Kép</th>
+                            <th><a href=\"?rendez=nev\">Név</a></th>
+                            <th><a href=\"?rendez=cegnev\">Cégnév</a></th>
+                            <th><a href=\"?rendez=mobil\">Mobil</a></th>
+                            <th><a href=\"?rendez=email\">Email</a></th>
+                            <th>Művelet</th>
+                        </tr>";
+        while ($sor = mysqli_fetch_array($eredmeny)) {  
+            $foto = htmlspecialchars($sor["foto"], ENT_QUOTES, "utf-8");
+            $nev = htmlspecialchars($sor["nev"], ENT_QUOTES, "utf-8");
+            $cegnev = htmlspecialchars($sor["cegnev"], ENT_QUOTES, "utf-8");
+            $mobil = htmlspecialchars($sor["mobil"], ENT_QUOTES, "utf-8");
+            $email = htmlspecialchars($sor["email"], ENT_QUOTES, "utf-8");
+            $kimenet .= "<tr>
+                            <td><img src=\"../kepek/{$foto}\"></td>
+                            <td>{$nev}</td>
+                            <td>{$cegnev}</td>
+                            <td>{$mobil}</td>
+                            <td>{$email}</td>
+                            <td><a href=\"torles.php?id={$sor['id']}\">Törlés</a> | <a href=\"modositas.php?id={$sor['id']}\">Módosítás</a></td>
+                        </tr>";
+        }
+        $kimenet .= "</table>";
+    }
+    $stmt -> close();
+}
+else {
+    echo"Hiba az elkészített utasítás létrehozásában" . mysqli_error( $dbconn );
+}
+
+?><!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../stilus.css">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Névjegyzék</h1>
+    <form method="post">
+        <input type="search" name="kifejezes" id="kifejezes">
+    </form>
+    <!--lapozó-->
+    <div class="container">
+        <!--kimenet ->adatbázisból beolvasott adatok-->
+        <?php //print $lapozo; ?>
+        <a href="felvitel.php">Új névjegy hozzáadása</a>
+        <?php print $kimenet; ?>
+    </div>
+</body>
+</html>
